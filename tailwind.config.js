08e1cd7159746d2452fe10b2fa27c1cbd7223538
marior-29 <maritoricotti@gmail.com>
2026-04-13 00:01:21 -0300

    feat: frontend boilerplate ready with shadcn, tailwind and main stack

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Shadcn necesita saber dónde están tus componentes y tu código
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
