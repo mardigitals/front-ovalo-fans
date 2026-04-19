849aafa88a2966478f14f163ec6126088508fb5a
marior-29 <maritoricotti@gmail.com>
2026-04-19 10:50:53 -0300

    creacion de termsconditions page con enrutado

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import DashboardLayout from '../components/layout/DashboardLayout';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
+import TermsConditionsPage from '@/pages/TermsConditionsPage';
 
 
 const AppRoutes = () => (
@@ -15,6 +16,7 @@ const AppRoutes = () => (
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/terms-conditions" element={<TermsConditionsPage />} />
 
       {/* Rutas Privadas (Dashboard) */}
       <Route path="/dashboard" element={<DashboardLayout />}>
