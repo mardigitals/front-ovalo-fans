464237f327ed6aeaa962e44d59490d812370e817
marior-29 <maritoricotti@gmail.com>
2026-07-09 10:39:58 -0300

    feat: correciones en sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Home, FileChartColumn, BadgeDollarSign, ShieldUser, Calendar, MapPin, Film, Gift, PenLine, Pyramid, Road, Users, Warehouse, User, LogOut, ChevronLeft, X, Shield, Camera, Star, Ticket, Settings, PercentIcon, FastForward, LockKeyholeOpen, Flag, Info, UserRoundKey, ShoppingBasket, Check, CircleCheckBig, ChartCandlestick, ShieldCheck } from 'lucide-react';
+import { Home, FileChartColumn, BadgeDollarSign, ShieldUser, Calendar, Film, Gift, PenLine, Pyramid, Road, Users, Warehouse, User, LogOut, ChevronLeft, X, Shield, Camera, Star, Ticket, Settings, PercentIcon, FastForward, LockKeyholeOpen, Flag, Info, UserRoundKey, ShoppingBasket, Check, ChartCandlestick, ShieldCheck } from 'lucide-react';
 import { useAuth } from '@/hooks/useAuth';
 import { useTheme } from '@/hooks/useTheme'; // <-- Importamos el hook
 
@@ -38,13 +38,11 @@ const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
     
     items.push({ to: '/dashboard/resumen', icon: <FileChartColumn size={20} />, label: 'Resumen' });
     items.push({ to: '/dashboard/mi-perfil', icon: <User size={20} />, label: 'Mi Perfil' });
-    // items.push({ to: '/dashboard/validar', icon: < CircleCheckBig size={20} />, label: 'Validar DNI' });
     items.push({ to: '/dashboard/mi-cuenta', icon: <ShieldCheck size={20} />, label: 'Mi Cuenta' });
     
 
     if (rol === 'fan') {
       items.push({ to: '/dashboard/pagos', icon: <BadgeDollarSign size={20} />, label: 'Mi Billetera' });
-      items.push({ to: '/dashboard/mapa', icon: <MapPin size={20} />, label: 'Mapa Interactivo' });
       items.push({ to: '/dashboard/beneficios', icon: <PercentIcon size={20} />, label: 'Beneficios' });
       items.push({ to: '/calendario', icon: <Ticket size={20} />, label: 'Recitales y Carreras' });
       items.push({ to: '/dashboard/pruebas', icon: <Road size={20} />, label: 'Pruebas' });
